76aa30d87dc0e5b78b41ad5d2b7e9783aea766c1
Nosungbin <sviin7713@sangilmedia.hs.kr>
2025-02-10 18:50:49 +0900

    개인용 플라스크 서버 파일 제거

--- app.py
+++ app.py
@@ -33,6 +33,16 @@ def signup():
 def mypage():
     return render_template('mypage.html')
 
+# 관리페이지 메인
+@app.route('/mgmt')
+def manager_page():
+    return render_template('manager_page_main.html')
+
+# 관리페이지 유저 관리
+@app.route('/mgmt_user')
+def manager_page_user():
+    return render_template('manager_page_user.html')
+
 # 수상내역
 @app.route('/awards')
 def awards():
