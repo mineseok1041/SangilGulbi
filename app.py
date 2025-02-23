c8bf7ef4344005adb0ff1c5b5526854e25274bed
YumMinSeok <minseok@sangilmedia.hs.kr>
2025-02-23 23:22:03 +0900

    app.py 합칠때 오류 수정

--- app.py
+++ app.py
@@ -86,21 +86,6 @@ def manager_page_user():
 def manager_page_add():
     return render_template('manager_page_add.html')
 
-# 관리페이지 메인
-@app.route('/mgmt')
-def manager_page():
-    return render_template('manager_page_main.html')
-
-# 관리페이지 학생 관리
-@app.route('/mgmt_user')
-def manager_page_user():
-    return render_template('manager_page_user.html')
-
-# 관리페이지 관리자 추가
-@app.route('/mgmt_add')
-def manager_page_add():
-    return render_template('manager_page_add.html')
-
 # 수상내역
 @app.route('/awards')
 def awards():
