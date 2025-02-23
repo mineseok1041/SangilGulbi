32d92f5da8d3cfb8d463c7dcc8b1994b0ac70a99
leekinam <leekinam20@sangilmedia.hs.kr>
2025-02-24 00:34:09 +0900

    로그인 코드 주석 추가

--- static/js/login.js
+++ static/js/login.js
@@ -1,6 +1,6 @@
 function togglePasswordVisibility(inputId, icon) {
     var input = document.getElementById(inputId);
-
+//아이콘 이미지 클릭시 password 타입을 text 형태로 바꿔 비밀 번호 표시
     if (input.type === "password") {
         input.type = "text";
         setTimeout(() => {
