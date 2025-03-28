36bbd99ad8e919d20ebad51497b3c68253fef0f6
siiiiieun <lion101027@gmail.com>
2025-03-28 14:14:36 +0900

    Design : 어워즈 헤더, 마이페이지, 로그인 대문자,

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -1,13 +1,27 @@
-const openPopup = () => {
+const info_openPopup = () => {
     // 팝업을 띄울 페이지 URL
-    var popupURL = "/mypage_Popup";
+    var popupURL = "mypage_InfoPopup.html";
     // 팝업 창의 속성
     var option = "width=600,height=400,scrollbars=yes";
     // 팝업 열기
     window.open(popupURL, "Popup", option);
-}
+  }
 
-const closePopup = () => {
+  const info_closePopup = () => {
     // 팝업 닫기
     window.close();
-}
+  }
+
+  const score_openPopup = () => {
+    // 팝업을 띄울 페이지 URL
+    var popupURL = "mypage_ScorePopup.html";
+    // 팝업 창의 속성
+    var option = "width=600,height=400,scrollbars=yes";
+    // 팝업 열기
+    window.open(popupURL, "Popup", option);
+  }
+
+  const score_closePopup = () => {
+    // 팝업 닫기
+    window.close();
+  }
