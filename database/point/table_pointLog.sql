02a586e6e03f74e504f85822253129ccf2b9c3e8
kihoon0713 <kxhxxn7713@sangilmedia.hs.kr>
2025-05-13 01:34:19 +0900

    DB pointLog table giveTeacherId 컬럼 오타 수정

--- database/point/table_pointLog.sql
+++ database/point/table_pointLog.sql
@@ -3,11 +3,9 @@ create table pointLog (
     type VARCHAR2(10) CHECK (type IN ('bonus', 'penalty')),
     studentId varchar2(12),
     writeTeacherId varchar2(12),
-    giveteacherId varchar2(12),
+    giveTeacherId varchar2(12),
     point number,
     reason varchar2(255),
     opinion varchar2(255),
     addDate varchar2(17) default to_char(sysdate, 'YYYYMMDD HH24:MI:SS')
 );
-
-
