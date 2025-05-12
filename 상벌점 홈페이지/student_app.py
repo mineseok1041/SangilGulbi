c01e3f24deb0192bec03e13db51f00b5a42452b0
mineseok1041 <minseok@sangilmedia.hs.kr>
2025-05-12 15:23:49 +0900

    Feat: 학생이 로그보이게 함

--- 상벌점 홈페이지/student_app.py
+++ 상벌점 홈페이지/student_app.py
@@ -2,10 +2,13 @@ from flask import Flask, redirect, render_template, session, url_for, Blueprint
 
 from usersSVC import usersSVC
 from usersDTO import usersDTO
+from pointSVC import pointSVC
+from pointLogDTO import pointLogDTO
 
 studentBlue = Blueprint('student', __name__, url_prefix='/student')
 
 usersSVC = usersSVC()
+pointSVC = pointSVC()
 
 @studentBlue.route('/')
 def index():
@@ -14,8 +17,11 @@ def index():
             return redirect(url_for('index'))
         
         respDTO = usersSVC.getUsersInfo(usersDTO(id=session['id']))
+        pointLogList = pointSVC.getPointLogByStdID(usersDTO(id=session['id']))
+        pointLogStudent = [usersSVC.getUsersInfo(usersDTO(id=log.studentId)) for log in pointLogList]
+        pointLogTeacher = [usersSVC.getUsersInfo(usersDTO(id=log.giveTeacherId)) for log in pointLogList]
 
-        return render_template('student/indexStudent.html', usersDTO=respDTO)
+        return render_template('student/indexStudent.html', usersDTO=respDTO, pointLogList=pointLogList, pointLogStudent=pointLogStudent, pointLogTeacher=pointLogTeacher)
     except Exception as e:
         print(e)
         return redirect(url_for('index'))
@@ -25,11 +31,18 @@ def pointLog():
     if 'id' not in session:
         return redirect(url_for('index'))
     
-    return render_template('student/pointLogStudent.html')
+    respDTO = usersDTO(id=session['id'], name=session['name'], stdNum=session['stdNum'], identity=session['identity'])
+    pointLogList = pointSVC.getPointLogByStdID(usersDTO(id=session['id']))
+    pointLogStudent = [usersSVC.getUsersInfo(usersDTO(id=log.studentId)) for log in pointLogList]
+    pointLogTeacher = [usersSVC.getUsersInfo(usersDTO(id=log.giveTeacherId)) for log in pointLogList]
+    
+    return render_template('student/pointLogStudent.html', usersDTO=respDTO, pointLogList=pointLogList, pointLogStudent=pointLogStudent, pointLogTeacher=pointLogTeacher)
 
 @studentBlue.route('/community')
 def community():
     if 'id' not in session:
         return redirect(url_for('index'))
+    
+    respDTO = usersDTO(id=session['id'], name=session['name'], stdNum=session['stdNum'], identity=session['identity'])
     
     return render_template('student/communityStudent.html')
