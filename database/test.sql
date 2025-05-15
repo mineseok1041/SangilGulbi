4ed3f41db4883923c2c3c158f165e44a01c79818
mineseok1041 <minseok@sangilmedia.hs.kr>
2025-05-15 10:10:50 +0900

    Fix: 상벌점 로그 이름길어도 들어가게 수정

--- database/test.sql
+++ database/test.sql
@@ -4,3 +4,9 @@ select * from pointLog;
 
 drop table users;
 drop SEQUENCE seq_users_no;
+
+ALTER TABLE pointLog MODIFY (
+    studentName VARCHAR2(50),
+    writeTeacherName VARCHAR2(50),
+    giveTeacherName VARCHAR2(50)
+);
