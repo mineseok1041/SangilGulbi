f1aacdfd4af0e3cbb2df0642890cd247df789b22
YumMinSeok <minseok@sangilmedia.hs.kr>
2025-07-04 23:03:14 +0900

    Test: 테스트코드 추가

---
+++ database/test/insert_testStudent.sql
@@ -0,0 +1,22 @@
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test5', 'test5', '테스터5', '14523', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test6', 'test6', '테스터6', '24501', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test7', 'test7', '테스터7', '35694', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test8', 'test8', '테스터8', '12487', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test9', 'test9', '테스터9', '31492', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test10', 'test10', '테스터10', '20148', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test11', 'test11', '테스터11', '17832', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test12', 'test12', '테스터12', '34452', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test13', 'test13', '테스터13', '13375', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test14', 'test14', '테스터14', '21460', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test15', 'test15', '테스터15', '19875', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test16', 'test16', '테스터16', '32218', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test17', 'test17', '테스터17', '23941', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test18', 'test18', '테스터18', '11475', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test19', 'test19', '테스터19', '31132', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test20', 'test20', '테스터20', '26170', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test21', 'test21', '테스터21', '15483', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test22', 'test22', '테스터22', '34325', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test23', 'test23', '테스터23', '22596', 2, 1);
+INSERT INTO users(id, password, name, stdNum, identity, verified) VALUES('test24', 'test24', '테스터24', '12940', 2, 1);
+
+commit;
