de4b8ebb9dcdffac8d3a1c7ffc51fe9b2922417b
nohseongbin <sviin7713@sangilmedia.hs.kr.com>
2025-07-20 23:11:40 +0900

    feat : 학생 페이지 반응형 완성 (내 정보 수정 제외)

--- 상벌점 홈페이지/static/js/student/indexStudent.js
+++ 상벌점 홈페이지/static/js/student/indexStudent.js
@@ -29,3 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.querySelector('.nav-toggle').addEventListener('click', function() {
+    document.querySelector('.sidebar').classList.toggle('active');
+});
